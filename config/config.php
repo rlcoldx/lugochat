6e90204ba292497ff882491974c8869d731c647b
Rafael Luiz <rafael@agenciaclose.com.br>
2024-09-15 00:08:21 -0300

    Projeto em 75%

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+define('DOMAIN', 'http://localhost/lugochat');
+define('PATH', 'http://localhost/lugochat');
+define('NAME', 'LUGO CHAT');
+define('PRODUCTION', FALSE);
+
+//// CONFIGURAÇÕES DO BANCO ########################
+define('HOST_MAIN', '177.234.145.178');
+define('USER_MAIN', 'rafael_lugochat');
+define('PASS_MAIN', 'wU$S2Fam]N~{V4Ffia');
+define('DBSA_MAIN', 'rafael_lugochat');
+
+/// Payment Config
+define('MP_PUBLIC_KEY', 'TEST-0fd4102d-9527-4276-aa89-08b749830e42');
+define('MP_PRIVATE_KEY', 'TEST-7344792450155289-042218-5e5f4ef71382577709e50c9b4273c034-270137611');
