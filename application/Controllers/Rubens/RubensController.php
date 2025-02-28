2228090343d27c8b7bd45e724f47d60092ec1f84
Rafael Luiz <rafael@agenciaclose.com.br>
2025-02-28 08:38:11 -0300

    ajustes

--- application/Controllers/Rubens/RubensController.php
+++ application/Controllers/Rubens/RubensController.php
@@ -20,4 +20,19 @@ class RubensController extends Controller
         // print_r($obj);
         // echo '</pre>';
     }
+
+    public function disponibilidade()
+    {
+        // $model = new Rubens;
+        // $suites = $model->getSuites()->getResult();
+        // $json_result = $suites[0]["json_result"];
+        
+        // Converte o JSON em objeto PHP
+        //echo $json_result;
+        // $obj = json_decode($json_result);
+        
+        echo '<pre>';
+        print_r($_REQUEST);
+        echo '</pre>';
+    }
 }
--- routes/API/rubens.php
+++ routes/API/rubens.php
@@ -1,3 +1,4 @@
 <?php
 $router->namespace("Agencia\Close\Controllers\Rubens");
 $router->get("/api/rubens/suites", "RubensController:suites");
+$router->get("/api/rubens/suite/disponibilidade", "RubensController:disponibilidade");
