33c52a708fb88955900d4c618f66200ee74d2cfd
Rafael Luiz <rafael@agenciaclose.com.br>
2025-04-08 15:24:47 -0300

    ajustes

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('DOMAIN', 'http://localhost/lugochat');
-define('PATH', 'http://localhost/lugochat');
+define('DOMAIN', 'https://buscademoteis.com.br/painel');
+define('PATH', 'https://buscademoteis.com.br/painel');
 define('NAME', 'LUGO CHAT');
 define('PRODUCTION', FALSE);
 
@@ -16,7 +16,7 @@ define('SIS_API', 'http://177.156.7.7:55005');
 define('SOFTHOUSE', 'e7ff638fe581513d36f9743936d83a90-8e6702fcc16d42059686a50ce429b52a');
 
 define('TOKEN', 'qgvCMRoHCTujhHswhiTyRALSY8QzRQyvlB0PWPqmuUpELHDf5vjirY04JQsaj3xx');
-define('ACCESSTOKEN', 'APP_USR-6619019503196756-111110-960e8da08d80ad993e73d4b3d423bd9f-263421911');
+define('ACCESSTOKEN', 'APP_USR-7344792450155289-042218-098d04ea1b43278887ef425218689d8f-270137611');
 
 @session_start();
 try {
