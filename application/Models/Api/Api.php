4894c8cf093a3be0b485cae8fe587e79c77a069d
Rafael Luiz <rafael@agenciaclose.com.br>
2026-01-21 16:00:41 -0300

    Ajustes API

--- application/Models/Api/Api.php
+++ application/Models/Api/Api.php
@@ -279,7 +279,7 @@ class Api extends Model
         $updateReserva->ExeUpdate(
             'reservas',
             [
-                'processado_api' => 'N',
+                'processado_api' => 'S',
                 'cancelada_api' => 'S',
                 'fase_api' => 0,
                 'status_reserva' => 'Cancelado'
